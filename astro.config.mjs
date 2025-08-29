e28d7a2dc204c91f6a80999c6a3954ac7a6ede01
Gumi_Lockhart <gumi.200010@gmail.com>
2025-08-29 21:51:45 +0900

    ax

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://gumi-21.github.io/",
-	base: "/gumi_blog",
+	site: "https://gumi-21.github.io",
+	base: "blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
