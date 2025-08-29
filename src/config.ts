c3a1cf38f286573f15d7c4eb63f2501f2c43b479
Gumi_Lockhart <gumi.200010@gmail.com>
2025-08-29 22:52:56 +0900

    blog-restart

--- src/config.ts
+++ src/config.ts
@@ -16,12 +16,12 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
+		enable: true,
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
+			enable: true, // Display the credit text of the banner image
+			text: "WELCOME", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/GUMI-21", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -54,25 +54,25 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Gumi",
+	bio: "THINK ONLY ABOUT YOUR ART",
 	links: [
 		{
 			name: "Twitter",
 			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://x.com/Khynpe50k02pwCt",
 		},
 		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "https://steamcommunity.com/id/1538025/",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/GUMI-21",
 		},
 	],
 };
---
+++ src/content/posts/article/测试-first.md
@@ -0,0 +1,14 @@
+---
+title: 重启
+published: 2025-08-29
+description: '花了一点时间重新用Astro部署了博客'
+image: 'https://i.pinimg.com/1200x/bc/a3/00/bca300886e006badb02ad04a255338e3.jpg'
+tags: [随笔]
+category: '文章'
+draft: false 
+lang: 'zh-CN'
+---
+
+## 重启
+上一次写博客还是三年前-刚毕业的时候，后来嫌麻烦，就只用Obisdian来把所有的笔记放在一起了，导致笔记也越来越臃肿。
+现在时间充裕了点，准备重新把自己有价值的笔记搬到博客上面，顺便记录自己的学习与生活。
